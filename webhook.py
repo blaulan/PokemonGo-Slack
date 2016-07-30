1f03bc78b16e27a12426c51d9621cb062c33e593
Yue Wu <me@blaulan.com>
2016-07-30 09:45:45 -0400

    reset logging settings

--- webhook.py
+++ webhook.py
@@ -38,10 +38,10 @@ if __name__ == '__main__':
     with open('config.json', 'r') as f:
         config = json.loads(f.read())
     if config['debug']:
-        logging.basicConfig(level=logging.DEBUG)
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.getLogger('alarm').setLevel(logging.DEBUG)
     else :
-        logging.getLogger('werkzeug').setLevel(logging.ERROR)
         logging.getLogger('requests').setLevel(logging.DEBUG)
         logging.getLogger('alarm').setLevel(logging.INFO)
 
-    app.run(debug=config['debug'],host=config['host'], port=config['port'])
+    app.run(debug=config['debug'], host=config['host'], port=config['port'])
